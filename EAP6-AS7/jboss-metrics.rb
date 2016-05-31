9ca34cc97164ac6dd5a05b4df1a1733c923d30da
Onbayev Kanat <okanat@localhost.localdomain>
2016-05-31 14:12:56 +0600

    Small Fix 2

--- EAP6-AS7/jboss-metrics.rb
+++ EAP6-AS7/jboss-metrics.rb
@@ -1,17 +1,5 @@
 #!/usr/bin/env ruby
-#
-# System VMStat Plugin
-# ===
-#
-# This plugin uses vmstat to collect basic system metrics, produces
-# Graphite formated output.
-#
-# Copyright 2011 Sonian, Inc <chefs@sonian.net>
-#
-# Released under the same terms as Sensu (the MIT license); see LICENSE
-# for details.
-#
-# rubocop:disable HandleExceptions
+
 
 require 'rubygems' if RUBY_VERSION < '1.9.0'
 require 'sensu-plugin/metric/cli'
